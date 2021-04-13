7fb4b1b4cff88d41864740814c69931773400504
feddelegrand7 <ihaddaden.fodeil@gmail.com>
2021-04-13 16:19:33 +0200

    Initial commit

---
+++ R/plainBar.R
@@ -0,0 +1,17 @@
+
+plainBar <- function(data,
+                     color = "orange",
+                     width = NULL,
+                     height = NULL,
+                     barPadding = 0.1) {
+  r2d3::r2d3(
+    data = data,
+    script = system.file("d3/barcharts/barchartPlain.js", package = "DScript"),
+    width = width,
+    height = height,
+    options = list(
+      color = color,
+      barPadding = barPadding
+      )
+  )
+}
---
+++ inst/d3/barcharts/barchartPlain.js
@@ -0,0 +1,26 @@
+let sortState = false;
+
+let xScale = d3
+  .scaleBand()
+  .domain(d3.range(data.length))
+  .range([0, width])
+  .paddingInner(options.barPadding);
+
+let yScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(data)])
+  .range([0, height]);
+
+svg.attr('id', 'mysvg')
+  .selectAll("rect")
+  .data(data)
+  .enter()
+  .append("rect")
+  .attr("x", (d, i) => xScale(i))
+  .attr("y", (d) => height - yScale(d))
+  .attr("width", xScale.bandwidth())
+  .attr("height", (d) => yScale(d))
+  .attr("fill", options.color)
+  .attr("stroke", "darkred");
+
+
