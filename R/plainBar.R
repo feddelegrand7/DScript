73b5435351487a4851bd33a56b89665a47d1f994
feddelegrand7 <ihaddaden.fodeil@gmail.com>
2021-04-13 16:40:10 +0200

    bar plain done

--- R/plainBar.R
+++ R/plainBar.R
@@ -5,6 +5,9 @@ plainBar <- function(data,
                      height = NULL,
                      barPadding = 0.1,
                      bgColor = "#FFFFFF") {
+
+
+
   r2d3::r2d3(
     data = data,
     script = system.file("d3/barcharts/barchartPlain.js", package = "DScript"),
